742df2d2ecf22841960e6eaad1660d727dfc3dcf
Rushikesh <rushikesh@dynamicvishva.in>
2025-09-17 20:45:18 +0530

    17-09-25

---
+++ syscoapp/urls.py
@@ -0,0 +1,10 @@
+from django.urls import path
+from django.conf import settings
+from django.conf.urls.static import static
+from syscoapp.views import *
+
+
+urlpatterns = [
+    path('',home)
+]+ static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+
